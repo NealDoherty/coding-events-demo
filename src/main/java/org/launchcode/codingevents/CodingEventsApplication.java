2bfc5b3d6ec32f9f153a4e3148756eb6ea02a4a4
NealDoherty <110205152+NealDoherty@users.noreply.github.com>
2022-11-17 16:28:11 -0600

    11/17 work to 17.3

--- src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
+++ src/main/java/org/launchcode/codingevents/CodingEventsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingEventsApplication {
+public class  CodingEventsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodingEventsApplication.class, args);
